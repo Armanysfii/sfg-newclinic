54d1e5e631e078c0ed2b6334b5c270cc38764a4a
arman <yousefiarman@yahoo.com>
2022-05-17 23:06:02 +0200

    push 1

---
+++ src/main/java/com/arman/spring/petclinicenhance/PetclinicEnhanceApplication.java
@@ -0,0 +1,13 @@
+package com.arman.spring.petclinicenhance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicEnhanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicEnhanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arman/spring/petclinicenhance/PetclinicEnhanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arman.spring.petclinicenhance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicEnhanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
